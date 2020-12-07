aff3b532121900af97152f497f15788e2d466d06
KobeH3 <m.mozdzierz@gmail.com>
2020-12-08 00:19:30 +0100

    8-12-20121

--- home.js
+++ home.js
@@ -1,4 +1,24 @@
-let welcome = 'eluwinka';
-console.log("Zmiana w tekscie");
-//komenty
-showMessage(welcome);
+// let name = prompt('Jak masz na imie?','wpisz imie');
+// let message = `Hello ${name}`;
+// messageLength = message.length;
+// console.log(message);
+// console.log(messageLength);
+// //komenty
+// showMessage(message);
+// // showMessage(messageLength);
+
+// let amount= 123;
+// amount = amount.toString();
+// console.log(typeof amount); 
+
+
+// let password;
+// while (password !='1234') {
+//     password= prompt('jakie jest hasło?');
+// };
+// console.log('udalo sie wejsc!');
+
+let message = 1;
+for (let i=1;i<=90;i=i*2) {
+    console.log(message++);
+};
